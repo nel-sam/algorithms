cbef2fce9f13194302511de6ccd88addcf9a789f
Nelson <nelson.rs.07@gmail.com>
2019-11-06 17:32:13 -0700

    Added FizzBuzz

--- other.js
+++ other.js
@@ -6,6 +6,22 @@ fibonacciRecursive = (n) => {
   return fibonacciRecursive(n -1) + fibonacciRecursive (n - 2);
 }
 
+fizzBuzz = (n) => {
+  let result = '';
+
+  for (i = 1; i <= n; i++) {
+    if (i % 3 === 0) {
+      result += 'Fizz';
+    }
+
+    if (i % 5 === 0) {
+      result += 'Buzz';
+    }
+  }
+
+  return result;
+}
+
 test = (result, expectedResult) => {
   if (result === expectedResult) {
     console.log('Pass');
@@ -16,3 +32,4 @@ test = (result, expectedResult) => {
 
 test(fibonacciRecursive(4), 3);
 test(fibonacciRecursive(6), 8);
+test(fizzBuzz(15), 'FizzBuzzFizzFizzBuzzFizzFizzBuzz');
